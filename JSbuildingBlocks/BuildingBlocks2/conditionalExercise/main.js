8fbdaa8e77ed434f6118a7fdcba37c3179adc384
Kyle <16170619+meestajones@users.noreply.github.com>
2020-12-10 21:30:54 -0700

    now learning returns 12/10

--- JSbuildingBlocks/BuildingBlocks2/conditionalExercise/main.js
+++ JSbuildingBlocks/BuildingBlocks2/conditionalExercise/main.js
@@ -1,12 +1,38 @@
-let season = prompt('What season is it right now?').toLowerCase();
 let response;
-
-if (season.indexOf('summer') !== -1){
-  response = "The sun is shining and it's time to hit the water park!";
-} else if (season.indexOf('winter') !== -1){
-  response = "Baby it's cold outside!";
+let machineActive = false;
+let input = document.querySelector('input');
+console.log(input.value);
+function run(){
+while (input.checked === false) {
+  response = alert('You need to turn the machine on first');
+   break;
+ } if (input.checked === true) {
+   let score = prompt('How many points did you score?');
+     if (score > 100 || score < 0) {
+       response = 'This is not possible, an error has occurred.';
+   } else if (score >= 0 && score <= 19) {
+       response = 'That was a terrible score -- total fail';
+   } else if (score >= 20 && score <= 39) {
+       response = 'You know some things, but its a pretty bad score.';
+   } else if (score >= 40 && score <= 69) {
+       response = 'You did a passable job, not bad';
+   } else if (score >= 70 && score <= 89) {
+       response = 'That\'s a great score, you really know your stuff'
+   } else if (score >= 90 && score <= 100) {
+       response = 'What an amazing score! Did you cheat? are you for real?'
+   } else if (score == undefined || score == null){
+       response = 'It doesn\'t seem like you even played the game! please try again';
 }
 
+const section = document.querySelector('section');
+
 let para1 = document.createElement('p');
-para1.textContent = response;
-document.body.appendChild(para1);
+let para2 = document.createElement('p');
+
+para1.textContent = `Your score is ` + score ;
+para2.textContent = response;
+
+section.appendChild(para1);
+section.appendChild(para2);
+};
+}
